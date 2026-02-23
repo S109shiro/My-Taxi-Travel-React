98b1b0007042bf55c09f7c0983240445eea49e01
Sebas <sebgonrod@gmail.com>
2026-02-22 22:57:20 -0500

    correcion name proyect

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/My-Taxi-Travel-React/"
+  base: "/my-taxi-travel-react/"
 })
