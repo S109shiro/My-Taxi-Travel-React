b416572f0d4090a70350a91ea827ec58a363b949
Sebas <sebgonrod@gmail.com>
2026-06-09 22:46:09 -0500

    Responsive commentary secicon home

--- src/pages/CommentsSection.jsx
+++ src/pages/CommentsSection.jsx
@@ -1,11 +1,10 @@
-// Importar estilos Tailwind y estilos unicos del componente
 import { useState } from "react";
 import "./commentsSection.css";
+import imgUser01 from "../assets/imgHome/user01.jpg"
+import imgUser02 from "../assets/imgHome/user02.jpg"
+import imgUser03 from "../assets/imgHome/user03.jpg"
 
 function CommentsSection({ commentsUsers = {} }) {
-  //console.log(commentsUsers);
-
-  // Estado para pasar los distintos comentarios
   const [currentIndex, setCurrentIndex] = useState(0);
 
   function nextComment(){
@@ -15,7 +14,6 @@ function CommentsSection({ commentsUsers = {} }) {
     return  currentIndex > 0 ? setCurrentIndex(currentIndex -1) : null;
   }
 
-  //console.log(currentIndex)
   return (
     <>
       <section className="text-gray-600 body-font">
@@ -23,215 +21,89 @@ function CommentsSection({ commentsUsers = {} }) {
           <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
             Reseñas de nuestros clientes
           </h1>
-          <div className="flex flex-3 -m-4">
-            <div className="p-4 md:w-1/2 w-full">
-              <div className="tarjeta h-full bg-gray-100 p-8 rounded shadow-lg"onClick={()=>{backComment()}} >
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 w-full md:w-1/2 lg:w-1/3">
+              <div className="tarjeta h-full bg-gray-100 p-8 rounded shadow-lg" onClick={()=>{backComment()}}>
                 <div className="estrella flex mb-2">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
                 </div>
                 <p className="leading-relaxed mb-6" onClick={()=>{console.log("Se cliquea el comentario");}}>
                   <i>
-                    “El servicio fue muy rápido, pedí el taxi y llegó en menos
+                    "El servicio fue muy rápido, pedí el taxi y llegó en menos
                     de 5 minutos. El conductor fue muy amable y el carro estaba
-                    limpio.”
+                    limpio."
                   </i>
                 </p>
                 <a className="inline-flex items-center">
                   <img
                     alt="testimonial"
-                    src="./img/home/user01.jpg"
+                    src={imgUser01}
                     className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
                   />
                   <span className="flex-grow flex flex-col pl-4">
-                    <span className="title-font font-medium text-gray-900">
-                      Mariana López
-                    </span>
+                    <span className="title-font font-medium text-gray-900">Mariana López</span>
                     <span className="text-gray-500 text-sm">29/08/2025</span>
                   </span>
                 </a>
               </div>
             </div>
-            <div className="p-4 md:w-1/2 w-full">
+            <div className="p-4 w-full md:w-1/2 lg:w-1/3">
               <div className="tarjeta h-full bg-gray-100 p-8 rounded shadow-lg">
                 <div className="estrella flex mb-2">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
                 </div>
                 <p className="leading-relaxed mb-6" onClick={()=>{console.log("Se cliquea el comentario");}}>
                   <i>
-                    “Excelente experiencia. Me sentí segura durante el viaje y
+                    "Excelente experiencia. Me sentí segura durante el viaje y
                     la opción de seguimiento en tiempo real me dio mucha
-                    tranquilidad.”
+                    tranquilidad."
                   </i>
                 </p>
                 <a className="inline-flex items-center">
                   <img
                     alt="testimonial"
-                    src="./img/home/user02.jpg"
+                    src={imgUser02}
                     className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
                   />
                   <span className="flex-grow flex flex-col pl-4">
-                    <span className="title-font font-medium text-gray-900">
-                      Carlos Méndez
-                    </span>
+                    <span className="title-font font-medium text-gray-900">Carlos Méndez</span>
                     <span className="text-gray-500 text-sm">29/08/2025</span>
                   </span>
                 </a>
               </div>
             </div>
-            <div className="p-4 md:w-1/2 w-full">
+            <div className="p-4 w-full md:w-1/2 lg:w-1/3">
               <div className="tarjeta h-full bg-gray-100 p-8 rounded shadow-lg" onClick={nextComment}>
                 <div className="estrella flex mb-2">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="black"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" />
-                  </svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><polygon points="12 1 15 8 22 9 17 15 19 22 12 18 5 22 7 15 2 9 9 8" /></svg>
                 </div>
                 <p className="leading-relaxed mb-6" onClick={()=>{console.log("Se cliquea el comentario");}}>
                   <i>
-                    “Muy buen servicio, lo uso casi todos los días para ir al
+                    "Muy buen servicio, lo uso casi todos los días para ir al
                     trabajo. Nunca he tenido inconvenientes y los conductores
-                    siempre son respetuosos.”
+                    siempre son respetuosos."
                   </i>
                 </p>
                 <a className="inline-flex items-center">
                   <img
                     alt="testimonial"
-                    src="./img/home/user03.jpg"
+                    src={imgUser03}
                     className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
                   />
                   <span className="flex-grow flex flex-col pl-4">
-                    <span className="title-font font-medium text-gray-900">
-                      Andrés Torres
-                    </span>
+                    <span className="title-font font-medium text-gray-900">Andrés Torres</span>
                     <span className="text-gray-500 text-sm">29/08/2025</span>
                   </span>
                 </a>
